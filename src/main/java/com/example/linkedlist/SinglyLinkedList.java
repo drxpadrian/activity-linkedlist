f06fcda3e6f2d2328d98d7da6982ca9972b3fd2d
drxpadrian <adrian.linares94@gmail.com>
2022-10-20 19:32:06 -0700

    done

---
+++ src/main/java/com/example/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,134 @@
+package com.example.linkedlist;
+
+import java.util.StringJoiner;
+
+public class SinglyLinkedList<E> implements LinkedList<E> {
+
+    private Node head;
+    private Node tail;
+
+    @Override
+    public void addFirst(E element) {
+
+        Node node = new Node(element, head); //1 & 2
+
+        if(head == null) {
+            head = node; //3
+            tail = head;
+        } else {
+            head = node; // 3
+        }
+
+    }
+
+    @Override
+    public void addLast(E element) {
+
+        Node node = new Node(element, null );//1 & 2
+
+        if (tail == null) {
+            tail = node; // 3
+            head = tail; // 3
+        } else {
+            tail.next = node; // 2
+            tail = node; // 3
+        }
+
+    }
+
+    @Override
+    public E pollFirst() {
+
+        E element;
+
+        if(head == null) {
+            element = null; // 1
+        } else {
+            element = head.element; // 1
+
+            Node next = head.next; // 2
+            head.next = null; // 3
+            head = next; // 4
+        }
+
+        return element;
+    }
+
+    @Override
+    public E pollLast() {
+        E element;
+        if (tail == null) {
+            element = null;
+        } else {
+
+            element = tail.element;
+
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+
+                Node current = head;
+                Node previous = head;
+                while (current.next != null) {
+                    previous = current;
+                    current = current.next;
+                }
+                tail = previous;
+                tail.next = null;
+            }
+        }
+
+        return element;
+    }
+
+    @Override
+    public E peekFirst() {
+        return head.element;
+    }
+
+    @Override
+    public E peekLast() {
+        return tail.element;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+        StringJoiner joiner = new StringJoiner(", ");
+        Node current = head;
+
+        while (current != null) {
+            joiner.add(current.element.toString());
+            current = current.next;
+        }
+
+        builder.append(joiner);
+        builder.append("]");
+
+        return builder.toString();
+    }
+
+
+
+    private class Node {
+        Node next;
+        E element;
+
+        public Node(E element, Node next) {
+            this.element = element;
+            this.next = next;
+
+        }
+    }
+}
